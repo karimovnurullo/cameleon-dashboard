5f1a17b24146d132fe303d826aa38f123de6e1a9
Nurullo <karimovdeveloper@gmail.com>
2023-06-11 00:52:29 +0500

    New commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## ( Portfolio )  https://www.nurullokarimov.uz
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+
+import "./main.scss";
+
+/**
+ *  packages
+ *  components
+ *  images
+ *  styles(css or sass)
+ */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
