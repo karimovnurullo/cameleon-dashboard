e7f25f5b029280a48aa387d85b6095bc2a8abb0f
Nurullo <karimovdeveloper@gmail.com>
2023-06-13 02:41:42 +0500

    New commit

--- src/components/performance/performance.jsx
+++ src/components/performance/performance.jsx
@@ -7,6 +7,7 @@ import show from "../../photos/performance/show.svg";
 import networkGreen from "../../photos/performance/network-green.svg";
 import networkRed from "../../photos/performance/network-red.svg";
 import chart from "../../photos/performance/chart.svg";
+import calendar from "../../photos/performance/calendar.svg";
 
 import "./performance.scss";
 
@@ -22,8 +23,16 @@ class Performance extends Component {
     const { cards } = this.state;
     return (
       <div className="performance">
-        <h3 className="performance-title">Site performance</h3>
-        <p className="performance-subtitle">Today vs 7 days ago</p>
+        <div className="performance-top">
+          <div>
+            <h3 className="performance-title">Site performance</h3>
+            <p className="performance-subtitle">Today vs 7 days ago</p>
+          </div>
+          <div className="date">
+            <img src={calendar} alt="" />
+            <p>June 03, 22 to July 02, 22</p>
+          </div>
+        </div>
         <div className="performance-cards">
           {cards.map(({ name, nameImg, num, miniNum, img }) => (
             <Card name={name} nameImg={nameImg} num={num} miniNum={miniNum} img={img} />
